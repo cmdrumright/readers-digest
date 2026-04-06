df6c24be95b1a129d4fc2ce3c57ce05ba8dd2a52
cmdrumright <cdrumright@protonmail.com>
2026-04-06 11:14:52 -0500

    Added models and ran migrations

---
+++ digestapi/models/book_category.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .book import Book
+from .category import Category
+
+
+class BookCategory(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="books")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="categories"
+    )
+    timestamp = models.DateTimeField()
---
+++ digestapi/models/review.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from .book import Book
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="review")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveIntegerField(
+        default=5, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    comment = models.CharField(max_length=200)
+    post_date = models.DateField()
